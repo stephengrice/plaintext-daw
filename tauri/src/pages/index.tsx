15be0622789fe0c1cab222a8b22eb28af21d7712
Stephen Grice <stephengrice@live.com>
2024-03-24 13:37:22 -0400

    Add back Tauri button handler invocation

--- tauri/src/pages/index.tsx
+++ tauri/src/pages/index.tsx
@@ -1,7 +1,18 @@
+'use client'
+
 import { Button, Title } from "@mantine/core";
+import { invoke } from '@tauri-apps/api/tauri';
 
 
 export default function Home() {
+  
+  const handleNew = () => {
+    console.log('new clicked');    
+  }
+  const handleOpen = async () => {
+    await invoke('open_project', {});
+  };
+
   return (
     <>
       <Title className='text-center pt-2'>Plaintext DAW</Title>
@@ -9,8 +20,8 @@ export default function Home() {
         Let's make some music.
       </div>
       <div className='flex w-full justify-around'>
-        <Button variant='outline' color='red'>New</Button>
-        <Button variant='outline'>Open</Button>
+        <Button variant='outline' color='red' onClick={() => handleNew()}>New</Button>
+        <Button variant='outline' onClick={() => handleOpen()}>Open</Button>
       </div>
     </>
   )
